b4ae9679e71d2b30364b733464b8a655735dab04
Mohamed Omar <mo6247911@gmail.com>
2025-02-25 17:24:31 -0800

    hi

--- Agrivision.Backend/Agrivision.Backend.Application/Models/ApplicationUserModel.cs
+++ Agrivision.Backend/Agrivision.Backend.Application/Models/ApplicationUserModel.cs
@@ -10,6 +10,7 @@ public class ApplicationUserModel : IApplicationUser
 {
     public string Id { get; set; } = string.Empty;
     public string UserName { get; set; } = string.Empty;
+
     public string Email { get; set; } = string.Empty;
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
